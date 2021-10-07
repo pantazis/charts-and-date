3e7b9e5d57deb9205f1c1a1ceeb3be69182b1b96
pantazis <pantazisvastardis@yahoo.com>
2021-10-07 11:24:28 +0300

    chart

---
+++ chartdemo/chartconfig.js
@@ -0,0 +1,86 @@
+const chartConfig ={};
+chartConfig.chart1 =  {
+    type: 'bar',
+    data: data.chart1,
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart - Stacked'
+        },
+      },
+      responsive: true,
+      interaction: {
+        intersect: false,
+      },
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true
+        }
+      }
+    }
+  };
+
+//----------------------------------chart2-------------------------------------------------//
+
+
+
+chartConfig.chart2= {
+    type: 'line',
+    data: data.chart2,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart'
+        }
+      }
+    },
+  };
+
+  //----------------------------------------------chart3--------------------------//
+  chartConfig.chart3 = {
+    type: 'doughnut',
+    data: data.chart3,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Doughnut Chart'
+        }
+      }
+    },
+  };
+  //----------------------------------chart4-----------------------------------------------
+
+  chartConfig.chart4 = {
+    type: 'line',
+    data: data.chart4 ,
+    options: {
+      plugins: {
+        filler: {
+          propagate: false,
+        },
+        title: {
+          display: true,
+          text: (ctx) => 'Fill: ' + 'start'
+        }
+      },
+      interaction: {
+        intersect: false,
+      }
+    },
+    
+  };
+  console.log(data.chart4);
--- chartdemo/index.php
+++ chartdemo/index.php
@@ -7,12 +7,41 @@
     <title>Document</title>
 </head>
 <body>
+<canvas class="chart1" width="400" height="400"></canvas>
+<canvas class="chart2" width="400" height="400"></canvas>
+<canvas class="chart3" width="400" height="400"></canvas>
+<canvas class="chart4" width="400" height="400"></canvas>
+
+
+
+
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+<script type="text/javascript" src="data.js"></script>
+<script type="text/javascript" src="chartconfig.js"></script>
+
+<script>
 /*      
 Stacked Bar Chart with Groups
 Line Chart
 Doughnut
 Line Chart Boundaries
 */
-    
+
+
+
+var chart1= $('.chart1')[0].getContext('2d');
+var myChart = new Chart(chart1,chartConfig.chart1);
+var chart2= $('.chart2')[0].getContext('2d');
+var myChart2 = new Chart(chart2,chartConfig.chart2);
+var chart3= $('.chart3')[0].getContext('2d');
+var myChart3 = new Chart(chart3,chartConfig.chart3);
+var chart4= $('.chart4')[0].getContext('2d');
+var myChart4 = new Chart(chart4,chartConfig.chart4);
+
+</script>    
 </body>
 </html>
